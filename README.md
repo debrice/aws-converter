302c78792d9e1cedbdde6a2d4106e35656f1c4ac
Brice Leroy <bbrriiccee@gmail.com>
2017-07-29 09:36:21 -0700

    singularity

--- README.md
+++ README.md
@@ -1,1 +1,42 @@
 # aws-converter
+
+This module provides the AWS converter which appeared in AWS 2.71.0. Since
+Lambda receive 2.54.0, you can't easily unmarshall records returned by
+dynamodb stream.
+
+This Library have been created to resolve this issue without having to
+manually add AWS-SDK within your bundle.
+
+## Usage
+
+```js
+import { marshall, unmarshall } from "aws-converter"
+
+
+marshall({
+  baz: { S: "foo bar" },
+  fooz: {
+    L: [
+      {
+        S: "foo",
+      },
+      {
+        N: "32.23",
+      },
+    ],
+  },
+})
+// {baz: "foo bar", fooz: ["foo", 32.23]}
+
+unmarshall({bar: { N: "32.23" }})
+// {bar: 32.23}
+
+```
+
+## Test
+
+Tests run under jest
+
+```
+yarn test
+```
---
+++ src/__tests__/unmarshall.js
@@ -0,0 +1,205 @@
+var unmarshall = require("../converter").unmarshall;
+
+describe("converter unmarshall", () => {
+  it("should unmarshall a simple streamed image", () => {
+    var streamImage = {
+      foo: { BOOL: true },
+      bar: { N: "32.23" },
+      baz: { S: "foo bar" },
+      fooz: {
+        L: [
+          {
+            S: "foo",
+          },
+          {
+            S: "bar",
+          },
+        ],
+      },
+    };
+
+    var model = {
+      foo: true,
+      bar: 32.23,
+      baz: "foo bar",
+      fooz: ["foo", "bar"],
+    };
+
+    expect(unmarshall(streamImage)).toEqual(model);
+  });
+
+  it("should unmarshall a complex streamed image", () => {
+    var streamImage = {
+      country: {
+        S: "United States",
+      },
+      number1: {
+        N: "0",
+      },
+      gender: {
+        S: "male",
+      },
+      city: {
+        S: "Los Angeles",
+      },
+      displayName: {
+        S: "Master Yoda",
+      },
+      latitude: {
+        N: "31.65468478",
+      },
+      number2: {
+        N: "4",
+      },
+      createdOn: {
+        S: "2017-07-25T23:26:14.545Z",
+      },
+      number3: {
+        N: "2",
+      },
+      aBoolean: {
+        BOOL: true,
+      },
+      number5: {
+        N: "8",
+      },
+      number6: {
+        N: "0",
+      },
+      countryCode: {
+        S: "US",
+      },
+      id: {
+        S: "54c52d33-9c02-43e0-a481-84d9f8cda4a0",
+      },
+      arrayOfM: {
+        L: [
+          {
+            M: {
+              countryCode: {
+                S: "FR",
+              },
+              name: {
+                S: "France",
+              },
+              type: {
+                S: "country",
+              },
+              createdOn: {
+                S: "2017-07-26T23:31:47.609Z",
+              },
+            },
+          },
+          {
+            M: {
+              countryCode: {
+                S: "US",
+              },
+              name: {
+                S: "United States of America",
+              },
+              type: {
+                S: "country",
+              },
+              createdOn: {
+                S: "2017-07-25T23:31:47.609Z",
+              },
+            },
+          },
+        ],
+      },
+      number7: {
+        N: "11",
+      },
+      longitude: {
+        N: "-114.15465",
+      },
+      mileage: {
+        N: "30",
+      },
+      profilePictureUrl: {
+        S: "https://penpals-media-profile-bucket.s3.amazonaws.com/type_image_54c52d33-9c02-43e0-a481-84d9f8cda4a0",
+      },
+      sentFromCountries: {
+        L: [
+          {
+            S: "US",
+          },
+        ],
+      },
+      friendCount: {
+        N: "1",
+      },
+      enqueued: {
+        BOOL: true,
+      },
+      phoneNumber: {
+        S: "+000987987",
+      },
+      dob: {
+        S: "1913-07-26T00:27:33.000Z",
+      },
+      interests: {
+        L: [
+          {
+            S: "pen pals",
+          },
+          {
+            S: "force",
+          },
+        ],
+      },
+      lastConversationReceived: {
+        N: "0",
+      },
+      status: {
+        S: "active",
+      },
+    };
+
+    var model = {
+      number5: 8,
+      aBoolean: true,
+      status: "active",
+      lastConversationReceived: 0,
+      country: "United States",
+      city: "Los Angeles",
+      displayName: "Master Yoda",
+      friendCount: 1,
+      interests: ["pen pals", "force"],
+      dob: "1913-07-26T00:27:33.000Z",
+      id: "54c52d33-9c02-43e0-a481-84d9f8cda4a0",
+      latitude: 31.65468478,
+      mileage: 30,
+      phoneNumber: "+000987987",
+      enqueued: true,
+      arrayOfM: [
+        {
+          name: "France",
+          type: "country",
+          createdOn: "2017-07-26T23:31:47.609Z",
+          countryCode: "FR",
+        },
+        {
+          name: "United States of America",
+          type: "country",
+          createdOn: "2017-07-25T23:31:47.609Z",
+          countryCode: "US",
+        },
+      ],
+      countryCode: "US",
+      gender: "male",
+      number6: 0,
+      profilePictureUrl: "https://penpals-media-profile-bucket.s3.amazonaws.com/type_image_54c52d33-9c02-43e0-a481-84d9f8cda4a0",
+      sentFromCountries: ["US"],
+      number1: 0,
+      number3: 2,
+      number7: 11,
+      createdOn: "2017-07-25T23:26:14.545Z",
+      longitude: -114.15465,
+      number2: 4,
+    };
+
+    expect(unmarshall(streamImage)).toEqual(model);
+  });
+});
